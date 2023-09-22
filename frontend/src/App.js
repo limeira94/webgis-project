da7e27a4dbfdd1095e5ed7f9f6d9a36074ed67d8
Felipe Limeira <limeira.felipe94@gmail.com>
2023-09-21 21:15:03 -0300

    chore(frontend): restore to main branch state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,44 +2,15 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Homepage';
 import Map from './components/Map';
-import MapComponent from './components/MapComponent';
-import UploadComponent from './components/UploadComponent';
-import './Caixa.css';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" exact element={<Home />} />
-//         <Route path="/map" exact element={<Map />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Visualizador de Mapa</h1>
-//       <MapComponent />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 
 function App() {
   return (
-    <div className="map-container">
-      <MapComponent lat={-20} lon={-50} />
-      <div className="caixa">
-        <h1>Adicione seu arquivo geojson</h1>
-        <UploadComponent />
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/map" exact element={<Map />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,88 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer, ZoomControl, LayersControl, Marker, Popup, GeoJSON } from 'react-leaflet';
+import { MapContainer, TileLayer, ZoomControl,LayersControl,Marker,Popup} from 'react-leaflet';
 import tileLayersData from './tileLayers.json';
 import './Map.css'
-import { useCallback } from 'react';
 
-function Map({ lat, lon, tileLayers }) {
-  return (
+function Map ({lat,lon,tileLayers}) {
+    return (
     <MapContainer className='map-container'
-      center={[lat, lon]}
-      zoom={10}
-      zoomControl={false}
-      maxZoom={20}
-      minZoom={2}>
-      <LayersControl position="bottomright">
-        {tileLayers.map((layer, index) => (
-          <LayersControl.BaseLayer checked name={layer.name} key={index}>
-            <TileLayer url={layer.url} key={index} />
-          </LayersControl.BaseLayer>
-        ))}
-      </LayersControl>
-      <ZoomControl position="bottomright" />
-      <Marker position={[lat, lon]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+                      center={[lat,lon]}
+                      zoom={5}
+                      zoomControl={false}
+                      maxZoom={20}
+                      minZoom={2}>
+        <LayersControl position="bottomright">
+          {tileLayers.map((layer, index) => (
+            <LayersControl.BaseLayer checked name={layer.name} key={index}>
+              <TileLayer url={layer.url} key={index}/>
+            </LayersControl.BaseLayer>
+          ))}
+        </LayersControl>
+        <ZoomControl position="bottomright"/>
+        <Marker position={[lat,lon]}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
     </MapContainer>
-  )
+    )
 }
 
-// const Homepage = () => {
+const Homepage = () => {
 
-//   const [userLocation, setUserLocation] = useState({ lat: 51.505, lon: -0.09 });
+    const [userLocation, setUserLocation] = useState({ lat: 51.505, lon: -0.09 });
 
-//   const tileLayers = tileLayersData.map((layer) => ({
-//     key: layer.key,
-//     name: layer.name,
-//     url: layer.url,
-//   }));
+    const tileLayers = tileLayersData.map((layer) => ({
+        key: layer.key,
+        name: layer.name,
+        url: layer.url,
+      }));
 
-//   const successCallback = useCallback((position) => {
-//     const { latitude, longitude } = position.coords;
-//     setUserLocation({ lat: latitude, lon: longitude });
-//   }, []);
+    const successCallback = (position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation({ lat: latitude, lon: longitude });
+        console.log(userLocation.lat,userLocation.lon)
+      };
 
-//   const errorCallback = (error) => {
-//     console.log(error);
-//   };
+    const errorCallback = (error) => {
+        console.log(error);
+      };
 
-//   useEffect(() => {
-//     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-//   }, [successCallback]);
-//   return (
-//     <>
-//       <Map tileLayers={tileLayers} lat={userLocation.lat} lon={userLocation.lon} />
-//     </>
-//   );
-// };
-
-// export default Homepage;
-
-
-const MapComponent = () => {
-  const [geojsonData, setGeojsonData] = useState(null);
-
-  useEffect(() => {
-    // Busque o arquivo GeoJSON do seu backend
-    fetch('http://127.0.0.1:8000/main/geojson/')
-      .then(response => response.json())
-      .then(data => {
-        setGeojsonData(data);
-      });
-  }, []);
-
-  return (
-    <MapContainer center={[-23.550520, -46.633308]} zoom={5} style={{ width: '100%', height: '400px' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {geojsonData && <GeoJSON data={geojsonData} />}
-    </MapContainer>
-  );
+    useEffect(() => {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    }, []); 
+return (
+    <>
+        <Map tileLayers={tileLayers} lat={userLocation.lat} lon={userLocation.lon}/>
+    </>
+);
 };
 
-export default MapComponent;
+export default Homepage;
