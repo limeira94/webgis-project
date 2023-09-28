4d8094a644c2296c2507ac2add260349ee71d15a
felipe <felipe_matheus.p@hotmail.com>
2023-09-28 14:48:44 -0300

    solving error

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -9,15 +9,15 @@ def render_react(request):
     return render(request, "index.html")
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
-    # re_path(r"^$", render_react),
-    # re_path(r"^(?:.*)/?$", render_react),
+    
     # # path('', views.HomePageView.as_view(), name='homepage'),
     path('api/main/upload/', views.GeoJSONFileUploadAPIView.as_view(), name='upload_geojson_api'),
     path('api/main/geojson/<int:pk>/', views.GeoJSONDetailView.as_view(), name='get_geojson'),
     path('api/main/geojson/', views.GeoJSONListView.as_view(), name='get_all_geojson'),
     path('api/main/register/', views.UserRegistrarionView.as_view(), name='user-register'),
     path('api/main/login/', views.LoginAPIView.as_view(), name='user-login'),
+    re_path(r"^$", render_react),
+    re_path(r"^(?:.*)/?$", render_react),
 ]
 
 
