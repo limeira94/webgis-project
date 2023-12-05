78a3f96cf5a8ecbee4470d79cde4c42024dcdd89
felipe <felipe_matheus.p@hotmail.com>
2023-12-05 14:53:49 -0300

    Projects selection working

--- frontend/src/components/NewProject.js
+++ frontend/src/components/NewProject.js
@@ -1,18 +1,17 @@
 import React, { useState,useEffect } from 'react';
-import NavbarComponent from './include/Navbar';
-import { useSelector } from 'react-redux';
-import { 
-  useNavigate 
-} from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+// import NavbarComponent from './include/Navbar';
+// import { useSelector } from 'react-redux';
+// import { 
+//   useNavigate 
+// } from 'react-router-dom';
+// import { useDispatch } from 'react-redux';
 import { MapComponent } from './utils/MapComponent';
 import M from 'materialize-css';
 
 function NewProject() {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const [confirmDelete, setConfirmDelete] = useState(false);
-    const { isAuthenticated, user, loading } = useSelector(state => state.user);
+    // const navigate = useNavigate();
+    // const dispatch = useDispatch();
+    // const { isAuthenticated, user, loading } = useSelector(state => state.user);
 
     const [rasters, setRasters] = useState([]);
     const [geojsons, setGeoJSONs] = useState([]); 
