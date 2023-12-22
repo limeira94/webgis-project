fd5f5c5d37ac2a7b8840b90340cf24ccbe6d5fc2
Felipe Limeira <limeira.felipe94@gmail.com>
2023-12-22 17:06:26 -0300

    style: adjusted about style

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,31 +1,48 @@
-
-
-import React, { useState, useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import 'materialize-css';
+import React from 'react';
 import Navbar from './include/Navbar';
 import Footer from './include/Footer';
-import M from 'materialize-css';
-import axios from 'axios'
-import Project from './Project';
-import './Homepage.css'
+import './About.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/'
+
+var url = process.env.PUBLIC_URL
 
 const About = () => {
-
     return (
-    <>
-        <Navbar/>
-        <div className='container'>
-            <h1>Developers</h1>
-            <p> Felipe 1</p>
-            <p> Felipe 2</p>
+        <>
+            <Navbar />
+            <div className='container'>
+                <h1 className="center-title">Developers</h1>
+                
+                <div className="row center-align">
+                    <div className="col s12 m6">
+                        <div className="card card-flat">
+                            <div className="card-image">
+                                <img src={url + "/felipe_pinto.jpg"} alt="Felipe Matheus Pinto"/>
+                            </div>
+                            <div className="card-content">
+                                <span className="card-title">Felipe Matheus Pinto</span>
+                                <p>Desenvolvedor GIS com experiência em Automações de processos, manipulação de Banco de dados e Desenvolvimento Web.</p>
+                            </div>
+                        </div>
+                    </div>
 
-        </div>
-        <Footer/>
-    </>
-);
+                    <div className="col s12 m6">
+                        <div className="card card-flat">
+                            <div className="card-image">
+                                <img src={url + "/felipe_limeira.png"} alt="Felipe Limeira"/>
+                            </div>
+                            <div className="card-content">
+                                <span className="card-title">Felipe Limeira</span>
+                                <p>Desenvolvedor GIS com experiência em Automações de processos, manipulação de Banco de dados e Desenvolvimento Web.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <Footer />
+        </>
+    );
 };
 
 export default About;
