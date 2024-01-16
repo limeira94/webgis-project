34efe39ccf15b2fa1b6398a8bb812a10b6141d61
felipe <felipe_matheus.p@hotmail.com>
2024-01-16 15:10:18 -0300

    solving small errors register

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -64,11 +64,13 @@ function Register() {
   var url = process.env.PUBLIC_URL
 
   return (
-    <div className='overlay-register room-register register-position'>
+
+    <div className='overlay'>
+      {/* overlay-register room-register register-position */}
 
       <div className="row">
         <div className="col s12 m5 offset-m4">
-          <div className="card-panel rounded-card ">
+          <div className="card-panel">
 
             <div className='center'>
               <a href="/" className="brand-logo">
@@ -81,7 +83,7 @@ function Register() {
               <form onSubmit={onSubmit}>
 
                 <div className='row'>
-                  <div className='input-field col s12'>
+                  <div className='input-field input-register col s12'>
                     <i className="material-icons prefix">account_circle</i>
                     <input
                       type="text"
@@ -94,7 +96,7 @@ function Register() {
                     <label htmlFor="icon_prefix">Username</label>
                   </div>
                 
-                  <div className='input-field col s12'>
+                  <div className='input-field input-register col s12'>
                     <i className="material-icons prefix">email</i>
                     <input
                       type="email"
@@ -106,7 +108,7 @@ function Register() {
                     <label htmlFor="email">Email</label>
                   </div>
 
-                <div className='input-field col s12'>
+                <div className='input-field input-register col s12'>
                   <i className="material-icons prefix">lock</i>
                   {/* <label htmlFor="password">Password</label> */}
                   <input
@@ -118,7 +120,7 @@ function Register() {
                   />
                   <label htmlFor="password">Password</label>
                 </div>
-                <div className='input-field col s12'>
+                <div className='input-field input-register col s12'>
                   <i className="material-icons prefix">lock</i>
                   {/* <label htmlFor="password2">Confirm Password</label> */}
                   <input
@@ -144,6 +146,99 @@ function Register() {
         </div>
       </div>
     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <div className='overlay-register room-register register-position'>
+
+    //   <div className="row">
+    //     <div className="col s12 m5 offset-m4">
+    //       <div className="card-panel rounded-card ">
+
+    //         <div className='center'>
+    //           <a href="/" className="brand-logo">
+    //             <img className="img-logo" src={url + "/logo.png"} alt="Web GIS Logo" height={100} />
+    //           </a>
+    //         </div>
+    //         <div className='container '>
+    //         <h3 className='center'>Register</h3>
+            
+    //           <form onSubmit={onSubmit}>
+
+    //             <div className='row'>
+    //               <div className='input-field col s12'>
+    //                 <i className="material-icons prefix">account_circle</i>
+    //                 <input
+    //                   type="text"
+    //                   // id="username"
+    //                   id="icon_prefix"
+    //                   name="username"
+    //                   value={formData.username}
+    //                   onChange={onChange}
+    //                 />
+    //                 <label htmlFor="icon_prefix">Username</label>
+    //               </div>
+                
+    //               <div className='input-field col s12'>
+    //                 <i className="material-icons prefix">email</i>
+    //                 <input
+    //                   type="email"
+    //                   id="email"
+    //                   name="email"
+    //                   value={formData.email}
+    //                   onChange={onChange}
+    //                 />
+    //                 <label htmlFor="email">Email</label>
+    //               </div>
+
+    //             <div className='input-field col s12'>
+    //               <i className="material-icons prefix">lock</i>
+    //               {/* <label htmlFor="password">Password</label> */}
+    //               <input
+    //                 type="password"
+    //                 id="password"
+    //                 name="password"
+    //                 value={formData.password}
+    //                 onChange={onChange}
+    //               />
+    //               <label htmlFor="password">Password</label>
+    //             </div>
+    //             <div className='input-field col s12'>
+    //               <i className="material-icons prefix">lock</i>
+    //               {/* <label htmlFor="password2">Confirm Password</label> */}
+    //               <input
+    //                 type="password"
+    //                 id="password2"
+    //                 name="password2"
+    //                 value={formData.password2}
+    //                 onChange={onChange}
+    //               />
+    //               <label htmlFor="password2">Confirm password</label>
+    //             </div>
+    //             {registerError && <p style={{ color: 'red' }}>{registerError}</p>}
+    //             <div className='center'>
+    //               <button className="waves-effect waves-light btn purple" type="submit">Create Account</button>
+    //             </div>
+    //             <div className='section center'>
+    //               Already have an account? <a href="/login" className="col s12">Sign in</a>
+    //             </div>
+    //             </div>
+    //           </form>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
