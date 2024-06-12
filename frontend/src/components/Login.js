cb7983d008f82438762ac156a4b1501be07d25a0
Felipe Limeira <limeira.felipe94@gmail.com>
2024-06-12 17:36:14 -0300

    change style login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,11 @@
-import React, { 
-    useState 
+import React, {
+  useState
 } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from './include/Navbar';
 import M from 'materialize-css';
 import '../styles/Login.css'
-import { 
+import {
   useDispatch
 } from 'react-redux';
 import {
@@ -13,110 +13,107 @@ import {
 } from '../features/user';
 
 const Login = () => {
-    const [formData,setFormData] = useState({
-        username:'',
-        password:'',
-    });
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    
-    const onSubmit = e => {
-      e.preventDefault();
-      dispatch(login(formData))
-        .then(data => {
-          if (data.meta.requestStatus==='rejected') {
-            const errors = Object.values(data.payload).flat();
-            errors.forEach(error => {
-              M.toast({
-                html: error,
-                classes: 'red rounded',
-                displayLength: 10000
-              });
-            })
-          } else {
-            M.toast(
-              {html: "Login sucessful", 
-               classes: 'green rounded',
-               displayLength:5000});
-            navigate("/");
-          }        
-        })
-        .catch(error => {
-          console.error('Login error:', error);
-        });
-    };
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+  const [hover, setHover] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const onChange = e => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-    var url = process.env.PUBLIC_URL
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch(login(formData))
+      .then(data => {
+        if (data.meta.requestStatus === 'rejected') {
+          const errors = Object.values(data.payload).flat();
+          errors.forEach(error => {
+            M.toast({
+              html: error,
+              classes: 'red rounded',
+              displayLength: 10000
+            });
+          })
+        } else {
+          M.toast(
+            {
+              html: "Login successful",
+              classes: 'green rounded',
+              displayLength: 5000
+            });
+          navigate("/");
+        }
+      })
+      .catch(error => {
+        console.error('Login error:', error);
+      });
+  };
 
-    return (
-      <>
-        <div className="overlay">
-          <div className="container section row rounded-card-login">
-          {/* center overlay room-login   */}
-            <div className="col s12 card card-login centered-element">
-            {/* row login-position centered-element back-color rounded-card-login */}
-              <div className='container section center'>
-                <a href="/" className="brand-logo">
-                  <img 
-                    className="img-logo" 
-                    src={url + "/logo.png"} 
-                    alt="Web GIS Logo" 
-                    height={100} 
-                  />
-                </a>
-              </div>
-            
-          
-              <div className="center">
-                <h4>
-                  <b>Login</b>
-                </h4>
-              </div>
-          
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-              <form className="col s12" onSubmit={onSubmit}>
-                <div className='row'>
-                  <div className='col s12'></div>
-                </div>
+  var url = process.env.PUBLIC_URL;
 
-                <div className='row'>
-                  <div className='input-field col s12'>
-                    <input className='validate' type='text' name='username' id='username' onChange={onChange}/>
-                    <label htmlFor='email'>Username</label>
-                  </div>
-                </div>
+  return (
+    <div className="login-container">
+      <div className="login-title">Entrar na plataforma</div>
+      <div className="login-card">
+        <div
+          className="login-image"
+          style={{
+            backgroundImage: hover
+              ? `url('${url}/map_login.svg')`
+              : `url('${url}/map_login2.svg')`
+          }}
+        ></div>
+        <div className="login-form-container">
+          <form onSubmit={onSubmit}>
+            <div className='input-field'>
+              <img
+                src={`${url}/icon_login.svg`}
+                alt="Username Icon"
+              />
+              <input
+                type='text'
+                name='username'
+                id='username'
+                placeholder='Usuário'
+                onChange={onChange}
+              />
+            </div>
 
-                <div className='row'>
-                  <div className='input-field col s12'>
-                    <input className='validate' type='password' name='password' id='password' onChange={onChange}/>
-                    <label htmlFor='password'>Password</label>
-                  </div>
-                  <label className="label-forgot">
-                    <a className='blue-text' href='/reset'><b>Forgot Password?</b></a>
-                  </label>
-                  <p>
-                    <a className='left' href='/register'>Don't have an account? <b>Register</b> now!</a>
-                  </p>
-                </div>
-                <br/>
-                <div className='row'>
-                  <button 
-                    type='submit' 
-                    // className='col s12 btn btn-large waves-effect login-button'
-                    className="col s12 btn btn-large login-button"
-                    >
-                      Login
-                  </button>
-                </div>
-            </form>
-          </div>
+            <div className='input-field'>
+              <img
+                src={`${url}/icon_password.svg`}
+                alt="Password Icon"
+              />
+              <input
+                type='password'
+                name='password'
+                id='password'
+                placeholder='Senha'
+                onChange={onChange}
+              />
+            </div>
+
+            <div className="login-links">
+              <a href='/register'>Crie sua conta!</a>
+              <a href='/reset'>Esqueceu a senha?</a>
+            </div>
+
+            <button
+              type='submit'
+              className='login-button'
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}
+            >
+              Entrar
+            </button>
+          </form>
         </div>
       </div>
-    </>
-
+    </div>
   );
 };
 
