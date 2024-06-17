3cde2e0ce3526fcd9f85f76c7848184846db439b
Felipe Limeira <limeira.felipe94@gmail.com>
2024-06-17 10:42:25 -0300

    change style register page

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { register } from '../features/user';
 import NavbarComponent from './include/Navbar';
@@ -7,138 +7,123 @@ import M from 'materialize-css';
 import '../styles/Register.css'
 
 function Register() {
-  const dispatch = useDispatch();
-  const { registered,  } = useSelector(state => state.user);
-  const [registerError, setRegisterError] = useState('');
-  const [formData,setFormData] = useState({
+    const dispatch = useDispatch();
+    const { registered, } = useSelector(state => state.user);
+    const [registerError, setRegisterError] = useState('');
+    const [formData, setFormData] = useState({
         username: '',
         email: '',
         password: '',
         password2: '',
-  })
-  useEffect(() => {
-    M.AutoInit();
-  }, []);
+    })
+    useEffect(() => {
+        M.AutoInit();
+    }, []);
 
-  const { username,email,password,password2 } = formData;
+    const { username, email, password, password2 } = formData;
 
-  const onChange = e => {
-    setFormData({...formData,[e.target.name]:e.target.value});
-  };
+    const onChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
 
-  const onSubmit = e => {
+    const onSubmit = e => {
         e.preventDefault();
-        dispatch(register({username,email,password,password2}))
-        .then(data=>{
-          if (data.meta.requestStatus==='rejected') {
-            const errors = Object.values(data.payload).flat();
-              setRegisterError(errors.join(' '));
-              errors.forEach(error => {
-                M.toast({
-                  html: error,
-                  classes: 'red rounded',
-                  displayLength: 10000
-                });
-              })
-          } else {
-            M.toast({
-              html: "Successfully registered",
-              classes: 'green rounded',
-              displayLength: 5000
-            });         
-          }        
-        })
-        .catch(error => {
-          console.error('Login error:', error);
-    });};
+        dispatch(register({ username, email, password, password2 }))
+            .then(data => {
+                if (data.meta.requestStatus === 'rejected') {
+                    const errors = Object.values(data.payload).flat();
+                    setRegisterError(errors.join(' '));
+                    errors.forEach(error => {
+                        M.toast({
+                            html: error,
+                            classes: 'red rounded',
+                            displayLength: 10000
+                        });
+                    })
+                } else {
+                    M.toast({
+                        html: "Successfully registered",
+                        classes: 'green rounded',
+                        displayLength: 5000
+                    });
+                }
+            })
+            .catch(error => {
+                console.error('Login error:', error);
+            });
+    };
 
-  if (registered) return <Navigate to='/login'/>;
+    if (registered) return <Navigate to='/login' />;
 
-  var url = process.env.PUBLIC_URL
+    var url = process.env.PUBLIC_URL
 
-  return (
+    return (
+        <div className="register-container">
+            <div className="register-card">
+                <div className="center">
+                    <a href="/" className="brand-logo">
+                        <img className="img-logo" src={`${url}/logo-world.svg`} alt="Web GIS Logo" height={100} />
+                    </a>
+                </div>
+                <div className="register-form-container">
+                    <div className="register-title">Cadastro</div>
+                    <form onSubmit={onSubmit}>
+                        <div className='input-field'>
 
-    <div className='overlay'>
+                            <input
+                                type='text'
+                                name='username'
+                                id='username'
+                                placeholder='Usuário'
+                                value={username}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className='input-field'>
 
-      <div className="row">
-        <div className="col s12 m5 offset-m4">
-          <div className="card-panel">
+                            <input
+                                type='email'
+                                name='email'
+                                id='email'
+                                placeholder='Email'
+                                value={email}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className='input-field'>
 
-            <div className='center'>
-              <a href="/" className="brand-logo">
-                <img className="img-logo" src={url + "/logo.png"} alt="Web GIS Logo" height={100} />
-              </a>
+                            <input
+                                type='password'
+                                name='password'
+                                id='password'
+                                placeholder='Senha'
+                                value={password}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className='input-field'>
+
+                            <input
+                                type='password'
+                                name='password2'
+                                id='password2'
+                                placeholder='Confirmar senha'
+                                value={password2}
+                                onChange={onChange}
+                            />
+                        </div>
+                        {registerError && <p style={{ color: 'red' }}>{registerError}</p>}
+                        <div className="center">
+                            <button className="waves-effect waves-light btn red rounded-button" type="submit">Criar conta</button>
+                        </div>
+                        <div className="section center">
+                            Já tem uma conta? <a href="/login">Entrar</a>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className='container '>
-            <h3 className='center'>Register</h3>
-            
-              <form onSubmit={onSubmit}>
-
-                <div className='row'>
-                  <div className='input-field input-register col s12'>
-                    <i className="material-icons prefix">account_circle</i>
-                    <input
-                      type="text"
-                      // id="username"
-                      id="icon_prefix"
-                      name="username"
-                      value={formData.username}
-                      onChange={onChange}
-                    />
-                    <label htmlFor="icon_prefix">Username</label>
-                  </div>
-                
-                  <div className='input-field input-register col s12'>
-                    <i className="material-icons prefix">email</i>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      value={formData.email}
-                      onChange={onChange}
-                    />
-                    <label htmlFor="email">Email</label>
-                  </div>
-
-                <div className='input-field input-register col s12'>
-                  <i className="material-icons prefix">lock</i>
-                  {/* <label htmlFor="password">Password</label> */}
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={onChange}
-                  />
-                  <label htmlFor="password">Password</label>
-                </div>
-                <div className='input-field input-register col s12'>
-                  <i className="material-icons prefix">lock</i>
-                  {/* <label htmlFor="password2">Confirm Password</label> */}
-                  <input
-                    type="password"
-                    id="password2"
-                    name="password2"
-                    value={formData.password2}
-                    onChange={onChange}
-                  />
-                  <label htmlFor="password2">Confirm password</label>
-                </div>
-                {registerError && <p style={{ color: 'red' }}>{registerError}</p>}
-                <div className='center'>
-                  <button className="waves-effect waves-light btn purple" type="submit">Create Account</button>
-                </div>
-                <div className='section center'>
-                  Already have an account? <a href="/login" className="col s12">Sign in</a>
-                </div>
-                </div>
-              </form>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
-}
+    );
+};
 
 export default Register;
